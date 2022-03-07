5eb1e1e2064136ee55cf8f21a83982dfd54adea0
akenO8 <akeno8@163.com>
2022-03-07 11:13:39 +0800

    从测试环境[Captain]拉下来的代码

--- 3.1/services/IMPALA/package/scripts/impala-catalog.py
+++ 3.1/services/IMPALA/package/scripts/impala-catalog.py
@@ -10,11 +10,11 @@ class ImpalaCatalog(ImpalaBase):
         # Install packages listed in metainfo.xml
         self.install_packages(env)
 
-        cmd = 'yum-config-manager --add-repo  ' \
-              'http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/cloudera-cdh5.repo'
+        #cmd = 'yum-config-manager --add-repo  ' \
+        #      'http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/cloudera-cdh5.repo'
 
-        Execute('echo "Running ' + cmd + '"')
-        Execute(cmd)
+        #Execute('echo "Running ' + cmd + '"')
+        #Execute(cmd)
 
 
         cmd = 'yum -y install  impala-server impala-catalog impala-state-store impala-shell'
